2f5078351bae86c4ba8609978827b53ece44478b
Cole0609 <cah062003@gmail.com>
2025-01-02 18:39:53 -0500

    very rough dos scan

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,11 @@ use std::{
     os::unix::process::ExitStatusExt,
     path::Path,
     process::Command,
+    time::Instant,
 };
 
-const SEG_SIG: i32 = 11;
+const SEG_SIG: i32 = 11; // seg fault
+const FPE_SIG: i32 = 8; // floating point exception
 const MAGIC_NUMBERS: [(usize, u8); 11] = [
     (1, 255),
     (1, 255),
@@ -30,8 +32,12 @@ const MAGIC_NUMBERS: [(usize, u8); 11] = [
 #[command(version, about, long_about = None)]
 struct Args {
     // image path
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "images/Canon_40D.jpg")]
     path: String,
+
+    // mutation rate
+    #[arg(short, long, default_value_t = 0.01)]
+    mutation_rate: f64,
 
     // number of attempts
     #[arg(short, long, default_value_t = 10_000)]
@@ -47,6 +53,27 @@ fn initialize(path: &str) -> io::Result<(ThreadRng, Vec<u8>)> {
     let rng: ThreadRng = rand::thread_rng();
     let mut data: Vec<u8> = vec![];
     utils::get_file_contents(&mut data, path)?;
+
+    let crash_dir = Path::new("crashes/");
+    let dos_dir = Path::new("dos");
+
+    // create crash dir
+    if fs::exists("crashes/").unwrap_or(false) {
+        fs::remove_dir_all(crash_dir)?;
+        fs::create_dir(crash_dir)?;
+    } else {
+        fs::create_dir(crash_dir)?;
+    }
+
+    // create dos dir
+    if fs::exists("dos/").unwrap_or(false) {
+        fs::remove_dir_all(dos_dir)?;
+        fs::create_dir(dos_dir)?;
+    } else {
+        fs::create_dir(dos_dir)?;
+    }
+
+    // create dos dir
     Ok((rng, data))
 }
 
@@ -55,8 +82,8 @@ fn initialize(path: &str) -> io::Result<(ThreadRng, Vec<u8>)> {
     clippy::cast_possible_truncation,
     clippy::cast_sign_loss
 )]
-fn apply_bitflip(rng: &mut ThreadRng, data: &mut [u8]) {
-    let mutation_num = (((data.len() as f64) - 4.0) * 0.01) as i64;
+fn apply_bitflip(rng: &mut ThreadRng, data: &mut [u8], mutation_rate: f64) {
+    let mutation_num = (((data.len() as f64) - 4.0) * mutation_rate) as i64;
     let mut indices = vec![];
 
     // collect indices
@@ -87,34 +114,43 @@ fn magic(rng: &mut ThreadRng, data: &mut [u8]) {
     }
 }
 
-fn mutate(rng: &mut ThreadRng, data_buf: &mut [u8]) -> io::Result<&'static str> {
+fn mutate(
+    rng: &mut ThreadRng,
+    data_buf: &mut [u8],
+    mutation_rate: f64,
+) -> io::Result<&'static str> {
     // manipulate jpg, save the method used for statistics if it does produce a crash
     let fuzz_method = if rng.gen_bool(0.5) {
-        apply_bitflip(rng, data_buf);
+        apply_bitflip(rng, data_buf, mutation_rate);
         "bitflip"
     } else {
         magic(rng, data_buf);
         "magic"
     };
-    // write mainpulated data to a temp mutate file
+    // write manipulated data to a temp mutate file
     utils::write_to_file(data_buf, "images/mutate.jpg")?;
 
     Ok(fuzz_method)
 }
 
+fn handle_dos(data: &[u8], index: u32, method: &str) -> io::Result<()> {
+    let path = format!("dos/dos.{method}.{index}.jpg");
+    utils::write_to_file(data, &path)?;
+    Ok(())
+}
+
 fn handle_crash(data: &[u8], index: u32, method: &str) -> io::Result<()> {
-    if !fs::exists("crashes/").unwrap_or(false) {
-        fs::create_dir(Path::new("crashes/"))?;
-    }
     let path = format!("crashes/crash.{method}.{index}.jpg");
     utils::write_to_file(data, &path)?;
-
     Ok(())
 }
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
-    let mut total_crashes = 0;
+    let mut crash_counter = 0;
+    let mut dos_counter = 0;
+    let mut seg_fault_crashes = 0;
+    let mut floating_point_crashes = 0;
     let mut bitflip_crashes = 0;
     let mut magic_crashes = 0;
 
@@ -133,28 +169,54 @@ fn main() -> io::Result<()> {
 
         // reset buffer and mutate it slightly once again
         mutate_buffer.copy_from_slice(&data);
-        let fuzz_method = mutate(&mut rng, &mut mutate_buffer)?;
+        let fuzz_method = mutate(&mut rng, &mut mutate_buffer, args.mutation_rate)?;
 
         // execute command
-        let output = Command::new("./binaries/exif")
-            .args(["images/mutate.jpg"])
+        let now = Instant::now();
+        let output = Command::new("binaries/ok-jpg-size")
+            // .args(["images/mutate.jpg"])
             .output()?;
-        let output_signal = output.status.signal().unwrap_or(0);
+        let elapsed_time = now.elapsed();
 
-        // if signal == 11, a crash occurred
-        if output_signal == SEG_SIG {
+        // check for dos
+        if elapsed_time.as_secs() > 2 {
+            handle_dos(&mutate_buffer, i, fuzz_method)?;
+            dos_counter += 1;
+        }
+
+        // uncomment for debug
+        if i == 0 {
+            println!(
+                "stdout for first attempt: {}",
+                String::from_utf8_lossy(&output.stdout)
+            );
+        }
+
+        if let Some(signal) = output.status.signal() {
             handle_crash(&mutate_buffer, i, fuzz_method)?;
 
-            if fuzz_method == "bitflip" {
-                bitflip_crashes += 1;
-            } else {
-                magic_crashes += 1;
+            // stats stuff
+            match signal {
+                SEG_SIG => seg_fault_crashes += 1,
+                FPE_SIG => floating_point_crashes += 1,
+                _ => println!("Unknown signal encountered: {signal}"),
             }
-            total_crashes += 1;
+            match fuzz_method {
+                "bitflip" => bitflip_crashes += 1,
+                "magic" => magic_crashes += 1,
+                _ => println!("this message should not be printed"),
+            }
+            crash_counter += 1;
         }
     }
     println!(
-        "\rFuzzing finished. Total crashes: {total_crashes}.\nBitflip crashes: {bitflip_crashes}\nMagic crashes: {magic_crashes}"
+        "\rFuzzing finished
+Total crashes             : {crash_counter}
+Total denials of service  : {dos_counter}
+Segmentation faults       : {seg_fault_crashes}
+Floating point exceptions : {floating_point_crashes}
+Bitflip crashes           : {bitflip_crashes}
+Magic crashes             : {magic_crashes}"
     );
 
     // Create reports
