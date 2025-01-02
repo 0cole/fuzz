046fcc26758799dbddb0e7f569f97e8b65fefb32
Cole0609 <cah062003@gmail.com>
2025-01-01 21:44:08 -0500

    general improvements, splitting up main

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,21 @@ use std::{
     process::Command,
 };
 
+const SEG_SIG: i32 = 11;
+const MAGIC_NUMBERS: [(usize, u8); 11] = [
+    (1, 255),
+    (1, 255),
+    (1, 127),
+    (1, 0),
+    (2, 255),
+    (2, 0),
+    (4, 255),
+    (4, 0),
+    (4, 128),
+    (4, 64),
+    (4, 127),
+];
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -27,12 +42,20 @@ struct Args {
     triage: bool,
 }
 
+fn initialize(path: &str) -> io::Result<(ThreadRng, Vec<u8>)> {
+    // init rng and get file contents once
+    let rng: ThreadRng = rand::thread_rng();
+    let mut data: Vec<u8> = vec![];
+    utils::get_file_contents(&mut data, path)?;
+    Ok((rng, data))
+}
+
 #[allow(
     clippy::cast_precision_loss,
     clippy::cast_possible_truncation,
     clippy::cast_sign_loss
 )]
-fn bitflip_data(rng: &mut ThreadRng, data: &mut [u8]) {
+fn apply_bitflip(rng: &mut ThreadRng, data: &mut [u8]) {
     let mutation_num = (((data.len() as f64) - 4.0) * 0.01) as i64;
     let mut indices = vec![];
 
@@ -51,31 +74,32 @@ fn bitflip_data(rng: &mut ThreadRng, data: &mut [u8]) {
 }
 
 fn magic(rng: &mut ThreadRng, data: &mut [u8]) {
-    let magic_numbers = [
-        (1, 255),
-        (1, 255),
-        (1, 127),
-        (1, 0),
-        (2, 255),
-        (2, 0),
-        (4, 255),
-        (4, 0),
-        (4, 128),
-        (4, 64),
-        (4, 127),
-    ];
-
     let len = data.len() - 8;
     let chosen_index = rng.gen_range(0..len);
 
-    let choice = rng.gen_range(0..magic_numbers.len());
-    let (num_magic_bytes, value) = magic_numbers[choice];
+    let choice = rng.gen_range(0..MAGIC_NUMBERS.len());
+    let (num_magic_bytes, value) = MAGIC_NUMBERS[choice];
 
     let mut counter = 0;
     while counter < num_magic_bytes {
         data[chosen_index + counter] = value;
         counter += 1;
     }
+}
+
+fn mutate(rng: &mut ThreadRng, data_buf: &mut [u8]) -> io::Result<&'static str> {
+    // manipulate jpg, save the method used for statistics if it does produce a crash
+    let fuzz_method = if rng.gen_bool(0.5) {
+        apply_bitflip(rng, data_buf);
+        "bitflip"
+    } else {
+        magic(rng, data_buf);
+        "magic"
+    };
+    // write mainpulated data to a temp mutate file
+    utils::write_to_file(data_buf, "images/mutate.jpg")?;
+
+    Ok(fuzz_method)
 }
 
 fn handle_crash(data: &[u8], index: u32, method: &str) -> io::Result<()> {
@@ -94,10 +118,11 @@ fn main() -> io::Result<()> {
     let mut bitflip_crashes = 0;
     let mut magic_crashes = 0;
 
-    // init rng and get file contents once
-    let mut rng: ThreadRng = rand::thread_rng();
-    let mut data: Vec<u8> = vec![];
-    utils::get_file_contents(&mut data, &args.path)?;
+    // init rng and data from input image
+    let (mut rng, data) = initialize(&args.path)?;
+
+    // this is a mutable buffer that will be reset after every iteration
+    let mut mutate_buffer = vec![0u8; data.len()];
 
     for i in 0..args.attempts {
         // update status
@@ -106,19 +131,9 @@ fn main() -> io::Result<()> {
             io::stdout().flush()?;
         }
 
-        let mut data_clone = data.clone();
-        let mut crash_method = "";
-
-        // manipulate jpg
-        if rng.gen_bool(0.5) {
-            bitflip_data(&mut rng, &mut data_clone);
-            crash_method = "bitflip";
-        } else {
-            magic(&mut rng, &mut data_clone);
-            crash_method = "magic";
-        }
-        // write mainpulated data to a temp mutate file
-        utils::write_to_file(&data_clone, "images/mutate.jpg")?;
+        // reset buffer and mutate it slightly once again
+        mutate_buffer.copy_from_slice(&data);
+        let fuzz_method = mutate(&mut rng, &mut mutate_buffer)?;
 
         // execute command
         let output = Command::new("./binaries/exif")
@@ -126,26 +141,27 @@ fn main() -> io::Result<()> {
             .output()?;
         let output_signal = output.status.signal().unwrap_or(0);
 
-        // 11 == segfault
-        if output_signal == 11 {
-            handle_crash(&data_clone, i, crash_method)?;
+        // if signal == 11, a crash occurred
+        if output_signal == SEG_SIG {
+            handle_crash(&mutate_buffer, i, fuzz_method)?;
 
-            if crash_method == "bitflip" {
+            if fuzz_method == "bitflip" {
                 bitflip_crashes += 1;
             } else {
                 magic_crashes += 1;
             }
-
             total_crashes += 1;
         }
     }
     println!(
-        "Program finished. Total crashes: {total_crashes}.\nBitflip crashes: {bitflip_crashes}\nMagic crashes: {magic_crashes}"
+        "\rFuzzing finished. Total crashes: {total_crashes}.\nBitflip crashes: {bitflip_crashes}\nMagic crashes: {magic_crashes}"
     );
 
     // Create reports
     if args.triage {
+        println!("Beginning triaging...");
         triage::triage_crashes()?;
+        println!("Finished triaging... Ending program.");
     }
     Ok(())
 }
